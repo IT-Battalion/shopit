0d49c43cd9648d9e6631675ea21620905ab88746
Arizona <37452656+ArizonaOnGitHub@users.noreply.github.com>
2021-09-29 11:43:49 +0200

    added libs

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,7 @@ window._ = require('lodash');
 try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
+    window.dt = require( 'datatables.net' )( window, $ );
 
     require('bootstrap');
 } catch (e) {}
@@ -39,3 +40,24 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+require( 'jszip' );
+require( 'pdfmake' );
+require( 'datatables.net-dt' )();
+require( 'datatables.net-autofill-dt' )();
+require( 'datatables.net-buttons-dt' )();
+require( 'datatables.net-buttons/js/buttons.colVis.js' )();
+require( 'datatables.net-buttons/js/buttons.html5.js' )();
+require( 'datatables.net-buttons/js/buttons.print.js' )();
+require( 'datatables.net-colreorder-dt' )();
+require( 'datatables.net-datetime' )();
+require( 'datatables.net-fixedcolumns-dt' )();
+require( 'datatables.net-fixedheader-dt' )();
+require( 'datatables.net-keytable-dt' )();
+require( 'datatables.net-responsive-dt' )();
+require( 'datatables.net-rowgroup-dt' )();
+require( 'datatables.net-rowreorder-dt' )();
+require( 'datatables.net-scroller-dt' )();
+require( 'datatables.net-searchbuilder-dt' )();
+require( 'datatables.net-searchpanes-dt' )();
+require( 'datatables.net-select-dt' )();
