0949a00ceaad43bc5d2404dbc64ba526801a0aca
Patrick <47031872+pelias006@users.noreply.github.com>
2021-10-04 15:21:02 +0200

    added Seeds added docker-compose.yml config

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Traits\UuidKey;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use LdapRecord\Laravel\Auth\LdapAuthenticatable;
@@ -13,7 +14,7 @@ use App\Models\Product;
 
 class User extends Authenticatable implements LdapAuthenticatable
 {
-    use Notifiable, AuthenticatesWithLdap, UuidKey, HasLdapUser;
+    use Notifiable, AuthenticatesWithLdap, UuidKey, HasLdapUser, HasFactory;
 
     /**
      * The attributes that are mass assignable.
