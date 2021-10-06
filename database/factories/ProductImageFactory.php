89e55f7f237be5b81594ab228a73bf945122bea9
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-10-06 15:10:50 +0200

    weird error at ProductFactory.php
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- database/factories/ProductImageFactory.php
+++ database/factories/ProductImageFactory.php
@@ -2,19 +2,18 @@
 
 namespace Database\Factories;
 
-use App\Models\Product;
 use App\Models\ProductImage;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class ProductFactory extends Factory
+class ProductImageFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = Product::class;
+    protected $model = ProductImage::class;
 
     /**
      * Define the model's default state.
@@ -25,14 +24,10 @@ class ProductFactory extends Factory
     {
         $user = User::factory();
         return [
-            'name' => $this->faker->name,
-            'description' => $this->faker->text,
-            'thumbnail' => ProductImage::factory(),
-            'price' => $this->faker->numberBetween(1, 300),
-            'sale' => $this->faker->numberBetween(0, 100),
-            'available' => $this->faker->numberBetween(0, 1000),
+            'path' => $this->faker->filePath(),
+            'type' => 'IMG_JPG',
             'created_by' => $user,
-            'updated_by' => $user
+            'updated_by' => $user,
         ];
     }
 }
