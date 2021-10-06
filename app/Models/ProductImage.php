ebe73276cef0a1427998e9a651ecbb67e2fa1f8f
pdamianik <39028343+pdamianik@users.noreply.github.com>
2021-10-06 21:19:44 +0200

    :construction: Add an IDE helper for type hinting in the model
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -10,6 +10,28 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
+/**
+ * App\Models\ProductImage
+ *
+ * @property string $id
+ * @property string $path
+ * @property string $type
+ * @property User|null $created_by
+ * @property User|null $updated_by
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductImage newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductImage newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductImage query()
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductImage whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductImage whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductImage whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductImage wherePath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductImage whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductImage whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ProductImage whereUpdatedBy($value)
+ * @mixin \Eloquent
+ */
 class ProductImage extends Model
 {
     use UuidKeyAndTrackInteractionUsers, HasFactory;
