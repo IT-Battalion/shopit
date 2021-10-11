c92e51e5ec2cd3ca2882ae1b252ba62de4d19460
pdamianik <39028343+pdamianik@users.noreply.github.com>
2021-10-11 15:43:13 +0200

    :construction: Logout disabled users
    A middleware will check whether a user got disabled while using the application and gets logged out if so.
    
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\LogoutDisabledUsers;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -38,6 +39,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\Authenticate::class,
+            LogoutDisabledUsers::class,
         ],
 
         'api' => [
