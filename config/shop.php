a98465c5beb79539ece31ac20f3fa8acb6e84729
pdamianik <39028343+pdamianik@users.noreply.github.com>
2021-10-11 12:47:00 +0200

    :construction: Typo
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

--- config/shop.php
+++ config/shop.php
@@ -12,7 +12,7 @@ return [
     | This value is used to delete invoices that don't have to be retained
     | any more. In Austria this period starts with the end of the year in
     | which the payment was done and ends after 7 years e.g. an invoice
-    | from January the 3rd 2013 has to be kept until Dezember the 31st 2020.
+    | from January the 3rd 2013 has to be kept until December the 31st 2020.
     */
 
     'invoice_retention_period' => 7,
