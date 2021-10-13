3d3f63b2675821160d7ed8643df83d0e9408053e
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-10-13 15:16:58 +0200

    tried something with Models
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -9,10 +11,17 @@ class UserController extends Controller
     protected $middleware = 'admin';
 
     public function ban(Request $request) {
+        $user = $request['user']; //can be email, username
+        $db_user = (new User)->where('username', $user)->orWhere('email', $user);
+        $db_user->enabled = false;
+        $db_user->update();
 
     }
 
     public function unban(Request $request) {
-
+        $user = $request['user']; //can be email, username
+        $db_user = (new User)->where('username', $user)->orWhere('email', $user);
+        $db_user->enabled = true;
+        $db_user->update();
     }
 }
