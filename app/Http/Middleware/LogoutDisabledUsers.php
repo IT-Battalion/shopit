2fb5e232061c55471a5ebc546349246d598325e3
pdamianik <39028343+pdamianik@users.noreply.github.com>
2021-10-15 17:37:56 +0200

    :construction: Fix LogoutDisabledUsers middleware
    Issue: The middleware tried to get the logged-in user on the login page -> no user was authenticated -> error
    
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

--- app/Http/Middleware/LogoutDisabledUsers.php
+++ app/Http/Middleware/LogoutDisabledUsers.php
@@ -11,12 +11,16 @@ class LogoutDisabledUsers
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
+        if (! $request->hasSession() || ! $request->user()) {
+            return $next($request);
+        }
+
         $user = Auth::user();
 
         if (! $user->isEnabled())
