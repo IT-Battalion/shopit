46bd4ee53dbdb1f8667b4791bbb7c1b56688a6d1
pdamianik <39028343+pdamianik@users.noreply.github.com>
2021-10-17 18:52:09 +0200

    :construction: Add icons to the database schema
    Add the icon table and foreign id link it
    Remove unnecessary index
    
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

---
+++ database/migrations/2021_10_17_141808_create_icons.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIcons extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('icons', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('artist');
+            $table->string('provider');
+            $table->string('license');
+            $table->string('mimetype');
+            $table->timestamps();
+        });
+
+        Schema::table('product_categories', function (Blueprint $table) {
+           $table->foreignId('icon_id')->after('name')->constrained();
+        });
+
+        Schema::table('order_product_categories', function (Blueprint $table) {
+            $table->foreignId('icon_id')->after('name')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('icons');
+    }
+}
