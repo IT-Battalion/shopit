1d190e908d7ba6f0c59044a1b1b5edec9f2ee669
pdamianik <39028343+pdamianik@users.noreply.github.com>
2021-10-18 13:54:25 +0200

    :construction: Add Icon factory and seeder
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

--- app/Models/ShoppingCart.php
+++ app/Models/ShoppingCart.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\ShoppingCart
+ *
+ * @property int $user_id
+ * @property int $product_id
+ * @property int $count
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|ShoppingCart newModelQuery()
+ * @method static Builder|ShoppingCart newQuery()
+ * @method static Builder|ShoppingCart query()
+ * @method static Builder|ShoppingCart whereCount($value)
+ * @method static Builder|ShoppingCart whereCreatedAt($value)
+ * @method static Builder|ShoppingCart whereProductId($value)
+ * @method static Builder|ShoppingCart whereUpdatedAt($value)
+ * @method static Builder|ShoppingCart whereUserId($value)
+ * @mixin Eloquent
+ */
 class ShoppingCart extends Model
 {
     use HasFactory;
