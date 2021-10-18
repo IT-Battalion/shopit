44d77e5a038401624e05423e7dcefc3b2376ed4d
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-10-18 13:05:57 +0200

    added ShoppingCart Model added ShoppingCart Seeder added ShoppingCart Factory
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

---
+++ app/Models/ShoppingCart.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShoppingCart extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'count',
+    ];
+
+    protected $casts = [
+        'count' => 'integer'
+    ];
+
+    protected $table = 'shopping_cart';
+
+
+}
