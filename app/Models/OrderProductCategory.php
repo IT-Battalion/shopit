1193b9666b3a5792445e6be121e2d898bebe7330
pdamianik <39028343+pdamianik@users.noreply.github.com>
2021-10-18 13:09:41 +0200

    :construction: Add icons to categories
    Add one to one relationship zwischen ProductCategories and Icons.
    
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

--- app/Models/OrderProductCategory.php
+++ app/Models/OrderProductCategory.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
 
 /**
@@ -26,4 +27,9 @@ use Illuminate\Support\Carbon;
  * @mixin Eloquent
  */
 class OrderProductCategory extends ProductCategory
-{}
+{
+    public function products(): HasMany
+    {
+        return $this->hasMany(OrderProduct::class);
+    }
+}
