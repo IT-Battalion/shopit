18b5057de5376e8d77f3dd7a373dfb3909248e9f
pdamianik <39028343+pdamianik@users.noreply.github.com>
2021-10-18 13:09:59 +0200

    :construction: Initial icon service stuff
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

--- config/shop.php
+++ config/shop.php
@@ -4,17 +4,74 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Invoice retention Period
+    | Invoice Configuration
     |--------------------------------------------------------------------------
-    |
-    | In years.
-    |
-    | This value is used to delete invoices that don't have to be retained
-    | any more. In Austria this period starts with the end of the year in
-    | which the payment was done and ends after 7 years e.g. an invoice
-    | from January the 3rd 2013 has to be kept until December the 31st 2020.
+    | Config values for invoice
     */
 
-    'invoice_retention_period' => 7,
-    'delete_after_invoice_retention_period' => false,
+    'invoice' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Invoice retention Period
+        |--------------------------------------------------------------------------
+        |
+        | In years.
+        |
+        | This value is used to delete invoices that don't have to be retained
+        | any more. In Austria this period starts with the end of the year in
+        | which the payment was done and ends after 7 years e.g. an invoice
+        | from January the 3rd 2013 has to be kept until December the 31st 2020.
+        */
+
+        'invoice.invoice_retention_period' => 7,
+
+        /*
+        | Delete invoices after the invoice retention period runs out?
+        */
+
+        'invoice.delete_after_invoice_retention_period' => false,
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Icon APIs
+    |--------------------------------------------------------------------------
+    |
+    | Configure the icon API Service.
+    */
+
+    'icon' => [
+
+        /*
+        | The noun Project API
+        | This API uses Oauth 1a for Authentication.
+        | Client available via https://packagist.org/packages/maglr/noun-project-php-client
+        */
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | API key
+        |--------------------------------------------------------------------------
+        |
+        | The API key for the noun project.
+        | Get started at: https://thenounproject.com/developers/apps/
+        */
+
+        'api_key' => env('NOUN_PROJECT_API_KEY'),
+
+        /*
+        |--------------------------------------------------------------------------
+        | API Secret
+        |--------------------------------------------------------------------------
+        |
+        | The API key for the noun project.
+        | Get started at: https://thenounproject.com/developers/apps/
+        */
+
+        'api_secret' => env('NOUN_PROJECT_API_SECRET'),
+    ],
+
 ];
