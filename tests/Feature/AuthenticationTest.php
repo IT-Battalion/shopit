e6bad60fbadfccf507b33578555897d08bc67c26
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-10-19 09:04:38 +0200

    added Test Cases for Authentication
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -2,6 +2,46 @@
 
 use function Pest\Laravel\get;
 
-test('has authentication page', function () {
+/**
+ * Check if the Authentication is accessible
+ * without session or token
+ */
+it('has authentication page', function () {
     get('/login')->assertStatus(200);
+});
+
+/**
+ * Check if Authentication is skipped if token is set
+ */
+test('skipped if token is set', function () {
+
+});
+
+/**
+ * Check if Authentication is skipped if Session is set
+ */
+test('skipped if session is set', function () {
+
+});
+
+/**
+ * Check if User is redirected to correct page when
+ * he comes from a page but wasn't logged in
+ */
+test('is user redirected to correct page if he comes from page which requires beeing logged in', function () {
+
+});
+
+/**
+ * Check if login with correct credentials works
+ */
+test('login works with correct credentials', function () {
+
+});
+
+/**
+ * Check if login with incorrect credentials works
+ */
+test('login does not work with incorrect credentials', function () {
+
 });
