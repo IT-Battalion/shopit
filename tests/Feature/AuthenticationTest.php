c461466ae917a453445b6d2c2190b5de0ace3520
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-10-19 09:04:38 +0200

    added Test Cases for Authentication
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -66,3 +66,39 @@ test('login works with correct credentials', function () {
 test('login does not work with incorrect credentials', function () {
 
 });
+
+/**
+ * Check if Authentication is skipped if token is set
+ */
+test('skipped if token is set', function () {
+
+});
+
+/**
+ * Check if Authentication is skipped if Session is set
+ */
+test('skipped if session is set', function () {
+
+});
+
+/**
+ * Check if User is redirected to correct page when
+ * he comes from a page but wasn't logged in
+ */
+test('is user redirected to correct page if he comes from page which requires beeing logged in', function () {
+
+});
+
+/**
+ * Check if login with correct credentials works
+ */
+test('login works with correct credentials', function () {
+
+});
+
+/**
+ * Check if login with incorrect credentials works
+ */
+test('login does not work with incorrect credentials', function () {
+
+});
