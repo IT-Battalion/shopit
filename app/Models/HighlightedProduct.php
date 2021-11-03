988ddeece98b6daca7c01ed0fdbe5eb5b26e0a25
pdamianik <39028343+pdamianik@users.noreply.github.com>
2021-11-04 00:05:21 +0100

    :book: Model Documentation
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

--- app/Models/HighlightedProduct.php
+++ app/Models/HighlightedProduct.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Database\Factories\HighlightedProductFactory;
 use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Support\Carbon;
 
 /**
  * App\Models\HighlightedProduct
@@ -22,6 +24,11 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
  * @method static Builder|HighlightedProduct whereId($value)
  * @method static Builder|HighlightedProduct whereProductId($value)
  * @mixin Eloquent
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static HighlightedProductFactory factory(...$parameters)
+ * @method static Builder|HighlightedProduct whereCreatedAt($value)
+ * @method static Builder|HighlightedProduct whereUpdatedAt($value)
  */
 class HighlightedProduct extends Model
 {
--- app/Models/Icon.php
+++ app/Models/Icon.php
@@ -33,16 +33,22 @@ use Illuminate\Support\Carbon;
  * @method static Builder|Icon whereUpdatedAt($value)
  * @mixin Eloquent
  * @method static IconFactory factory(...$parameters)
+ * @property string $original_id
+ * @property string $path
+ * @method static Builder|Icon whereOriginalId($value)
+ * @method static Builder|Icon wherePath($value)
  */
 class Icon extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'original_id',
         'name',
         'artist',
         'provider',
         'license',
         'mimetype',
+        'path',
     ];
 }
