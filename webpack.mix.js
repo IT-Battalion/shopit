ce6fd849290da4fcaced6eb041fc94522a06fbd0
Jan Franz Kammellander <jkammellander@student.tgm.ac.at>
2021-11-04 13:21:48 +0100

    configured webpack 👨🏽‍💻

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,3 +11,17 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss'),
+    ]);
+
+mix.js('resources/js/app.js', 'public/js')
+    .vue();
+
+mix.js('resources/js/app.js', 'public/js')
+    .extract(['vue'])
+
+mix.sass('resources/sass/app.scss', 'public/css');
+
+mix.browserSync('laravel.test');
