52474df86112a9d3c2143a0618283918d8adc6de
pdamianik <39028343+pdamianik@users.noreply.github.com>
2021-11-04 12:55:05 +0100

    Frontend changes
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,20 +1,6 @@
 window._ = require('lodash');
 
 /**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
-    window.dt = require( 'datatables.net' )( window, $ );
-
-    require('bootstrap');
-} catch (e) {}
-
-/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
@@ -43,21 +29,3 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 require( 'jszip' );
 require( 'pdfmake' );
-require( 'datatables.net-dt' )();
-require( 'datatables.net-autofill-dt' )();
-require( 'datatables.net-buttons-dt' )();
-require( 'datatables.net-buttons/js/buttons.colVis.js' )();
-require( 'datatables.net-buttons/js/buttons.html5.js' )();
-require( 'datatables.net-buttons/js/buttons.print.js' )();
-require( 'datatables.net-colreorder-dt' )();
-require( 'datatables.net-datetime' )();
-require( 'datatables.net-fixedcolumns-dt' )();
-require( 'datatables.net-fixedheader-dt' )();
-require( 'datatables.net-keytable-dt' )();
-require( 'datatables.net-responsive-dt' )();
-require( 'datatables.net-rowgroup-dt' )();
-require( 'datatables.net-rowreorder-dt' )();
-require( 'datatables.net-scroller-dt' )();
-require( 'datatables.net-searchbuilder-dt' )();
-require( 'datatables.net-searchpanes-dt' )();
-require( 'datatables.net-select-dt' )();
