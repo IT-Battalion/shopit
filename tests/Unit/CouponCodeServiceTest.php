95a46466bf62e95d04a1b55ea900a321e07a871c
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-04 14:56:58 +0100

    registered CouponService added CouponService Pesk Test
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

---
+++ tests/Unit/CouponCodeServiceTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\CouponCode;
+use App\Services\Coupons\CouponService;
+
+test('is coupon used', function () {
+    $service = app()->make(CouponService::class);
+    $code = CouponCode::whereEnabled(true)->get()->random();
+    expect($service->isUsed($code->code))->toBeTrue();
+});
+
+test('is coupon unused', function () {
+    $service = app()->make(CouponService::class);
+    $code = CouponCode::whereEnabled(false)->get()->random();
+    expect($service->isUsed($code->code))->toBeTrue();
+});
+
+test('change used status of coupon', function () {
+    $service = app()->make(CouponService::class);
+    $code = CouponCode::whereEnabled(false)->get()->random();
+    $service->markAsUsed($code->code);
+    expect($service->isUsed($code->code))->toBeTrue();
+});
+
+test('get coupon by code', function () {
+    $service = app()->make(CouponService::class);
+    $code = CouponCode::all()->random();
+    $code2 = $service->get($code->code);
+    expect(isset($code2))->toBeTrue();
+});
+
+test('create coupon code', function () {
+    $service = app()->make(CouponService::class);
+    $service->create('test', 20, now());
+    $code = CouponCode::whereCode('test')->get()->first();
+    expect(isset($code))->toBeTrue();
+});
