4ea052bae25a6df99ff5d99d09ced3142414b88f
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-08 14:08:45 +0100

    finished Stuff, Tests not working caus connection issue?
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- resources/lang/en/exceptionMessages.php
+++ resources/lang/en/exceptionMessages.php
@@ -5,9 +5,12 @@ return [
     'user_not_banned' => 'The given User is currently not banned.',
     'user_already_banned' => 'The given User is already banned.',
     'action_not_allowed_on_administrator' => 'This Action is not allowed to be performed on an Administrator.',
+    'product_not_found_for_deletion' => 'Product cannot be deleted. Not found.',
+    'product_not_found_for_image' => 'Image cannot be added to not existing Product.',
+    'product_not_found_for_attribute' => 'Attribute cannot be added to not existing Product',
+    'product_image_not_found_for_thumbnail' => 'Not existing image cannot be set as Thumbnail for a Product.',
     'category_not_found' => 'The Category could not be found.',
     'product_not_found' => 'The requested Product could not be found.',
-    'product_image_not_found_for_thumbnail' => 'Not existing image cannot be set as Thumbnail for a Product.',
     'coupon_code_discount_out_of_bounds' => 'The Discount of the Coupon must be between 1 and 100',
     'coupon_code_not_found_exception' => 'The Coupon for this code could not be found.',
 ];
