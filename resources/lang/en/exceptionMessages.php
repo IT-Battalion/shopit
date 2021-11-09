37021eaf29685db044b647d3ca9202ca07f99d4f
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-09 19:07:54 +0100

    fixed Tests fixed Product Model and related stuff fixed Product Migration missing ProductCategories
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- resources/lang/en/exceptionMessages.php
+++ resources/lang/en/exceptionMessages.php
@@ -6,6 +6,7 @@ return [
     'user_already_banned' => 'The given User is already banned.',
     'action_not_allowed_on_administrator' => 'This Action is not allowed to be performed on an Administrator.',
     'product_not_found_for_deletion' => 'Product cannot be deleted. Not found.',
+    'product_not_found_for_edit' => 'Product cannot be edited. Not found.',
     'product_not_found_for_image' => 'Image cannot be added to not existing Product.',
     'product_not_found_for_attribute' => 'Attribute cannot be added to not existing Product',
     'product_image_not_found_for_thumbnail' => 'Not existing image cannot be set as Thumbnail for a Product.'
--- tests/Unit/ProductServiceTest.php
+++ tests/Unit/ProductServiceTest.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Enums\AttributeType;
+use App\Models\Admin;
 use App\Models\Product;
 use App\Models\ProductCategory;
 use App\Services\Products\ProductService;
 
 test('create product', function () {
+    $this->actingAs(Admin::all()->random());
     $service = $this->app->make(ProductService::class);
     //wie haben ein problem. Man kann bei unit tests nicht acting as machen :( das geht bei artian commands und requests aber man kann keine methoden callen
     $product = $service->create('test', 33.33, 'The test product should be deleted soonTM', 20.00, ProductCategory::all()->random());
@@ -13,20 +15,13 @@ test('create product', function () {
 });
 
 test('create product images', function () {
+    $this->actingAs(Admin::all()->random());
     $service = $this->app->make(ProductService::class);
     $product = $service->get('test');
     $service->addImage('/path/', 'IMG/png', $product->id);
-    $service->addImage('/path/', 'IMG/png', $product->id);
+    $service->addImage('/path2/', 'IMG/png', $product->id);
     $service->addImage('/testpath/', 'IMG/png', $product->id);
-    expect($product->images_count)->toBeGreaterThan(0);
-});
-
-test('set thumbnail image', function () {
-    $service = $this->app->make(ProductService::class);
-    $product = $service->get('test');
-    $img = $service->getImage($product->images->first()->id);
-    $p = $service->setThumbnail($img, $product->name);
-    expect(is_null($p->thumbnail))->toBeFalse();
+    expect($product->images()->count())->toBeGreaterThan(0);
 });
 
 test('create product attributes', function () {
@@ -35,10 +30,61 @@ test('create product attributes', function () {
         'Blau:#0000ff',
         'Rot:#ff0000',
         'Grün:#00ff00',
-    ]), 'test');
-    $attributeType = $service->hasAttributeType(AttributeType::COLOR, 'test');
-    $attribute = $service->hasAttribute('Rot:#ff0000', 'test');
-    expect($attribute)->toBeTrue()->and($attributeType)->toBeTrue();
+    ]), null, 'test');
+    $service->addAttribute(AttributeType::COLOR, json_encode([
+        'White:#ffffff',
+    ]), null, 'test');
+    $attributeType = $service->hasAttributeType(AttributeType::COLOR, null, 'test');
+    $attribute = $service->hasAttribute('Rot:#ff0000', null, 'test');
+    expect($attributeType)->toBeTrue()
+        ->and($attribute)->toBeTrue();
+});
+
+test('set thumbnail image', function () {
+    $this->actingAs(Admin::all()->random());
+    $service = $this->app->make(ProductService::class);
+    $product = $service->get('test');
+    $img = $service->getImage($product->images->first()->id);
+    $p = $service->setThumbnail($img, null, $product->name);
+    expect(is_null($p->thumbnail))->toBeFalse();
+});
+
+test('edit Product', function () {
+    $this->actingAs(Admin::all()->random());
+    $service = $this->app->make(ProductService::class);
+    $service->edit(null, 'test', null, null, 'Edited Description');
+    expect($service->get('test')->description)->toEqual('Edited Description');
+});
+
+test('edit Product Image', function () {
+    $service = $this->app->make(ProductService::class);
+    $product = $service->get('test');
+    $image = $product->images()->first();
+    $service->editImage($image, 'editedPathXYZ');
+    expect($image->path)->toEqual('editedPathXYZ');
+});
+
+test('edit Product Attribute', function () {
+    $service = $this->app->make(ProductService::class);
+    $product = $service->get('test');
+    $attribute = $product->attributes()->first();
+    $service->editAttribute($attribute, AttributeType::VOLUME);
+    expect($attribute->type)->toEqual(AttributeType::VOLUME);
+});
+
+
+test('delete product attribute', function () {
+    $service = $this->app->make(ProductService::class);
+    $product = $service->get('test');
+    $service->removeAttribute($product->attributes()->first());
+    expect($product->attributes()->count())->toEqual(1);
+});
+
+test('delete product image', function () {
+    $service = $this->app->make(ProductService::class);
+    $product = $service->get('test');
+    $service->removeImage($product->images()->first());
+    expect($product->images()->count())->toEqual(2);
 });
 
 test('delete product', function () {
