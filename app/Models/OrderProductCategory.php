3198f65353ff118110da52771287100b308c1c82
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-10 18:19:20 +0100

    renewed ide-helper documentation added local scopes added relations
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- app/Models/OrderProductCategory.php
+++ app/Models/OrderProductCategory.php
@@ -5,34 +5,55 @@ namespace App\Models;
 use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Carbon;
 
 /**
  * App\Models\OrderProductCategory
  *
- * @property string $id
+ * @property int $id
  * @property string $name
+ * @property int $icon_id
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
+ * @property-read Icon|null $icon
  * @property-read Collection|OrderProduct[] $products
  * @property-read int|null $products_count
  * @method static Builder|OrderProductCategory newModelQuery()
  * @method static Builder|OrderProductCategory newQuery()
  * @method static Builder|OrderProductCategory query()
  * @method static Builder|OrderProductCategory whereCreatedAt($value)
+ * @method static Builder|OrderProductCategory whereIconId($value)
  * @method static Builder|OrderProductCategory whereId($value)
  * @method static Builder|OrderProductCategory whereName($value)
  * @method static Builder|OrderProductCategory whereUpdatedAt($value)
  * @mixin Eloquent
- * @property int $icon_id
- * @property-read Icon|null $icon
- * @method static Builder|OrderProductCategory whereIconId($value)
  */
-class OrderProductCategory extends ProductCategory
+class OrderProductCategory extends Model
 {
+    use HasFactory;
+
+    protected $table = 'order_product_categories';
+
+    protected $fillable = [
+        'name',
+        'icon_id',
+    ];
+
+    protected $casts = [
+        'icon_id' => 'integer',
+    ];
+
     public function products(): HasMany
     {
         return $this->hasMany(OrderProduct::class);
+    }
+
+    public function icon(): HasOne
+    {
+        return $this->hasOne(Icon::class);
     }
 }
