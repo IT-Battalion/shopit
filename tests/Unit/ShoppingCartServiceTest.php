2dc0d0d34aa7d3c87b4225952edb64d7c7bba7b0
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-10 13:27:17 +0100

    added interface functions added test functions
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- tests/Unit/ShoppingCartServiceTest.php
+++ tests/Unit/ShoppingCartServiceTest.php
@@ -1,5 +1,49 @@
 <?php
 
-test('example', function () {
-    expect(true)->toBeTrue();
+use App\Models\User;
+use App\Services\ShoppingCart\ShoppingCartService;
+
+test('add product to shopping cart', function () {
+    $service = $this->app->make(ShoppingCartService::class);
+    $this->actingAs(User::all()->first());
+});
+
+test('add non existing product to shopping cart', function () {
+    $service = $this->app->make(ShoppingCartService::class);
+    $this->actingAs(User::all()->first());
+});
+
+test('add product which is already existing to shopping cart', function () {
+    $service = $this->app->make(ShoppingCartService::class);
+    $this->actingAs(User::all()->first());
+});
+
+test('remove product from shopping cart', function () {
+    $service = $this->app->make(ShoppingCartService::class);
+    $this->actingAs(User::all()->first());
+});
+
+test('remove none existing product from shopping cart', function () {
+    $service = $this->app->make(ShoppingCartService::class);
+    $this->actingAs(User::all()->first());
+});
+
+test('remove 1 (amount) from shopping cart', function () {
+    $service = $this->app->make(ShoppingCartService::class);
+    $this->actingAs(User::all()->first());
+});
+
+test('get total price without taxes', function () {
+    $service = $this->app->make(ShoppingCartService::class);
+    $this->actingAs(User::all()->first());
+});
+
+test('get total price with taxes', function () {
+    $service = $this->app->make(ShoppingCartService::class);
+    $this->actingAs(User::all()->first());
+});
+
+test('get total price with coupon', function () {
+    $service = $this->app->make(ShoppingCartService::class);
+    $this->actingAs(User::all()->first());
 });
