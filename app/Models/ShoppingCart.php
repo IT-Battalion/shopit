e22f100ae7c7afbe0cd47692b91e68df9b6cb9be
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-10 18:09:57 +0100

    renewed ide-helper documentation added local scopes added relations
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- app/Models/ShoppingCart.php
+++ app/Models/ShoppingCart.php
@@ -7,30 +7,36 @@ use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOneOrMany;
 use Illuminate\Support\Carbon;
 
 /**
  * App\Models\ShoppingCart
  *
+ * @property int $id
  * @property int $user_id
  * @property int $product_id
  * @property int $count
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
+ * @method static ShoppingCartFactory factory(...$parameters)
  * @method static Builder|ShoppingCart newModelQuery()
  * @method static Builder|ShoppingCart newQuery()
  * @method static Builder|ShoppingCart query()
  * @method static Builder|ShoppingCart whereCount($value)
  * @method static Builder|ShoppingCart whereCreatedAt($value)
+ * @method static Builder|ShoppingCart whereId($value)
  * @method static Builder|ShoppingCart whereProductId($value)
  * @method static Builder|ShoppingCart whereUpdatedAt($value)
  * @method static Builder|ShoppingCart whereUserId($value)
  * @mixin Eloquent
- * @method static ShoppingCartFactory factory(...$parameters)
  */
 class ShoppingCart extends Model
 {
     use HasFactory;
+
+    protected $table = 'shopping_cart';
 
     protected $fillable = [
         'user_id',
@@ -39,10 +45,18 @@ class ShoppingCart extends Model
     ];
 
     protected $casts = [
-        'count' => 'integer'
+        'count' => 'integer',
+        'user_id' => 'integer',
+        'product_id' => 'integer',
     ];
 
-    protected $table = 'shopping_cart';
+    public function product(): HasOneOrMany
+    {
+        return $this->hasMany(Product::class, 'product_id', 'id');
+    }
 
-
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
