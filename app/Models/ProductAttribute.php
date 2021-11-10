1c5ec154e23d22d60ffc92a28bc4fe9001befcb8
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-10 18:21:58 +0100

    renewed ide-helper documentation added local scopes added relations
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- app/Models/ProductAttribute.php
+++ app/Models/ProductAttribute.php
@@ -7,32 +7,35 @@ use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Carbon;
 
 /**
  * App\Models\ProductAttribute
  *
- * @property string $product_id
- * @property int $type
- * @property string $values_available
- * @method static Builder|ProductAttribute newModelQuery()
- * @method static Builder|ProductAttribute newQuery()
- * @method static Builder|ProductAttribute query()
- * @method static Builder|ProductAttribute whereProductId($value)
- * @method static Builder|ProductAttribute whereType($value)
- * @method static Builder|ProductAttribute whereValuesAvailable($value)
- * @mixin Eloquent
  * @property int $id
+ * @property int $product_id
+ * @property int $type
+ * @property mixed $values_available
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  * @method static ProductAttributeFactory factory(...$parameters)
+ * @method static Builder|ProductAttribute newModelQuery()
+ * @method static Builder|ProductAttribute newQuery()
+ * @method static Builder|ProductAttribute query()
  * @method static Builder|ProductAttribute whereCreatedAt($value)
  * @method static Builder|ProductAttribute whereId($value)
+ * @method static Builder|ProductAttribute whereProductId($value)
+ * @method static Builder|ProductAttribute whereType($value)
  * @method static Builder|ProductAttribute whereUpdatedAt($value)
+ * @method static Builder|ProductAttribute whereValuesAvailable($value)
+ * @mixin Eloquent
  */
 class ProductAttribute extends Model
 {
     use HasFactory;
+
+    protected $table = 'product_attributes';
 
     protected $fillable = [
         'type',
@@ -44,4 +47,9 @@ class ProductAttribute extends Model
         'type' => 'integer',
         'product_id' => 'integer',
     ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
 }
