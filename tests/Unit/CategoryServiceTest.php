fbbcd02c75309f039d649d8f7821c47270547241
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-10 10:49:45 +0100

    added CategoryService.php implementation added CategoryServiceTest.php cases
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- tests/Unit/CategoryServiceTest.php
+++ tests/Unit/CategoryServiceTest.php
@@ -1,13 +1,23 @@
 <?php
 
-test('create category', function () {
+use App\Models\Icon;
+use App\Models\ProductCategory;
+use App\Services\Categories\CategoryService;
 
+test('create category', function () {
+    $service = $this->app->make(CategoryService::class);
+    $category = $service->create('test', Icon::all()->random());
+    expect(ProductCategory::whereName($category->name)->exists())->toBeTrue();
 });
 
 test('edit category', function () {
-
+    $service = $this->app->make(CategoryService::class);
+    $category = $service->edit('test', null, 'changedTest');
+    expect(ProductCategory::whereName('changedTest')->exists())->toBeTrue();
 });
 
 test('delete category', function () {
-
+    $service = $this->app->make(CategoryService::class);
+    $service->delete('changedTest');
+    expect(ProductCategory::whereName('changedTest')->exists())->toBeFalse();
 });
