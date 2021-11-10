57c19919b380bf18a4e0317a986cb2fd7e3b7a63
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-10 16:09:31 +0100

    renewed ide-helper documentation added local scopes added relations
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- app/Models/HighlightedProduct.php
+++ app/Models/HighlightedProduct.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Database\Factories\HighlightedProductFactory;
 use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -14,21 +13,20 @@ use Illuminate\Support\Carbon;
 /**
  * App\Models\HighlightedProduct
  *
- * @property string $id
- * @property string $product_id
- * @property-read Collection|Product[] $products
- * @property-read int|null $products_count
+ * @property int $id
+ * @property int $product_id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Product|null $products
+ * @method static HighlightedProductFactory factory(...$parameters)
  * @method static Builder|HighlightedProduct newModelQuery()
  * @method static Builder|HighlightedProduct newQuery()
  * @method static Builder|HighlightedProduct query()
+ * @method static Builder|HighlightedProduct whereCreatedAt($value)
  * @method static Builder|HighlightedProduct whereId($value)
  * @method static Builder|HighlightedProduct whereProductId($value)
- * @mixin Eloquent
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- * @method static HighlightedProductFactory factory(...$parameters)
- * @method static Builder|HighlightedProduct whereCreatedAt($value)
  * @method static Builder|HighlightedProduct whereUpdatedAt($value)
+ * @mixin Eloquent
  */
 class HighlightedProduct extends Model
 {
@@ -46,6 +44,6 @@ class HighlightedProduct extends Model
 
     public function products(): HasOne
     {
-        return $this->hasOne(Product::class);
+        return $this->hasOne(Product::class, 'product_id', 'id');
     }
 }
