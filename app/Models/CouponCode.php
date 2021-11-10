8a253fd6f1cd1733e79e475038d3e2ec6643be0d
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-10 16:08:17 +0100

    renewed ide-helper documentation added local scopes added relations
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- app/Models/CouponCode.php
+++ app/Models/CouponCode.php
@@ -2,41 +2,41 @@
 
 namespace App\Models;
 
-use Database\Factories\CouponCodeFactory;
 use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOneOrMany;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 /**
  * App\Models\CouponCode
  *
+ * @property int $id
+ * @property int $discount
+ * @property bool $enabled
+ * @property string $enabled_until
+ * @property string $code
+ * @property \App\Models\User|null $created_by
+ * @property \App\Models\User|null $updated_by
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static \Database\Factories\CouponCodeFactory factory(...$parameters)
  * @method static Builder|CouponCode newModelQuery()
  * @method static Builder|CouponCode newQuery()
  * @method static Builder|CouponCode query()
- * @mixin Eloquent
- * @property string $id
- * @property int $discount
- * @property int $enabled
- * @property string $enabled_until
- * @property string $code
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
  * @method static Builder|CouponCode whereCode($value)
  * @method static Builder|CouponCode whereCreatedAt($value)
+ * @method static Builder|CouponCode whereCreatedBy($value)
  * @method static Builder|CouponCode whereDiscount($value)
  * @method static Builder|CouponCode whereEnabled($value)
  * @method static Builder|CouponCode whereEnabledUntil($value)
  * @method static Builder|CouponCode whereId($value)
  * @method static Builder|CouponCode whereUpdatedAt($value)
- * @property-read User|null $created_by
- * @property-read User|null $updated_by
- * @method static Builder|CouponCode whereCreatedBy($value)
  * @method static Builder|CouponCode whereUpdatedBy($value)
- * @method static CouponCodeFactory factory(...$parameters)
+ * @mixin Eloquent
  */
 class CouponCode extends Model
 {
@@ -56,14 +56,14 @@ class CouponCode extends Model
         'updated_by' => 'integer',
     ];
 
-    public function created_by(): HasOne
+    public function created_by(): BelongsTo
     {
-        return $this->hasOne(User::class, 'id', 'created_by');
+        return $this->belongsTo(User::class, 'id', 'created_by');
     }
 
-    public function updated_by(): HasOne
+    public function updated_by(): BelongsTo
     {
-        return $this->hasOne(User::class, 'id', 'updated_by');
+        return $this->belongsTo(User::class, 'id', 'updated_by');
     }
 
     public function updateWith(User $user): CouponCode
@@ -88,5 +88,20 @@ class CouponCode extends Model
         static::updating(function (CouponCode $model) {
             if (!isset($model->updated_by)) $model->updateWith(Auth::user());
         });
+    }
+
+    public function orders_used(): HasOneOrMany
+    {
+        return $this->hasMany(Order::class, 'id', 'coupon_code_id');
+    }
+
+    public function scopeEnabled(Builder $query): Builder
+    {
+        return $query->where('enabled', '=', true);
+    }
+
+    public function scopeDisabled(Builder $query): Builder
+    {
+        return $query->where('enabled', '=', false);
     }
 }
