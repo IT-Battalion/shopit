6325a52c15b979035ec51bdefcec99d95f11d0f5
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-10 18:21:58 +0100

    renewed ide-helper documentation added local scopes added relations
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- app/Models/OrderProductAttribute.php
+++ app/Models/OrderProductAttribute.php
@@ -4,32 +4,50 @@ namespace App\Models;
 
 use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Carbon;
 
 /**
  * App\Models\OrderProductAttribute
  *
- * @property string $order_product_id
+ * @property int $id
+ * @property int $order_product_id
  * @property int $type
  * @property string $values_chosen
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
  * @method static Builder|OrderProductAttribute newModelQuery()
  * @method static Builder|OrderProductAttribute newQuery()
  * @method static Builder|OrderProductAttribute query()
- * @method static Builder|OrderProductAttribute whereOrderProductId($value)
- * @method static Builder|OrderProductAttribute whereType($value)
- * @method static Builder|OrderProductAttribute whereValuesChosen($value)
- * @mixin Eloquent
- * @property int $id
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
  * @method static Builder|OrderProductAttribute whereCreatedAt($value)
  * @method static Builder|OrderProductAttribute whereId($value)
+ * @method static Builder|OrderProductAttribute whereOrderProductId($value)
+ * @method static Builder|OrderProductAttribute whereType($value)
  * @method static Builder|OrderProductAttribute whereUpdatedAt($value)
+ * @method static Builder|OrderProductAttribute whereValuesChosen($value)
+ * @mixin Eloquent
  */
-class OrderProductAttribute extends ProductAttribute
+class OrderProductAttribute extends Model
 {
+    use HasFactory;
+
+    protected $table = 'order_product_attributes';
+
     protected $fillable = [
         'type',
-        'values_chosen',
+        'values_available',
+        'order_product_id',
     ];
+
+    protected $casts = [
+        'type' => 'integer',
+        'order_product_id' => 'integer',
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'order_product_id', 'id');
+    }
 }
