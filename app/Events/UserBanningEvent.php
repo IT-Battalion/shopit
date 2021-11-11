29aa12bf94fda12b921edb7c9d9c761c659f2a4f
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-11 09:45:50 +0100

    basic event stuff
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

---
+++ app/Events/UserBanningEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Admin;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserBanningEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Admin $model)
+    {
+        $model->banWith($model);
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
