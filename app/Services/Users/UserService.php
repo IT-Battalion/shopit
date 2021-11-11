e072739926e360c401e1a526c0acb85402208f90
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-11 09:45:50 +0100

    basic event stuff
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- app/Services/Users/UserService.php
+++ app/Services/Users/UserService.php
@@ -5,15 +5,11 @@ namespace App\Services\Users;
 use App\Exceptions\ActionNotAllowedForAdministratorExeption;
 use App\Exceptions\UserBannedException;
 use App\Exceptions\UserNotBannedException;
-use App\Models\Order;
-use App\Models\ShoppingCart;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Collection;
 
 class UserService implements UserServiceInterface
 {
     /**
-     * @inheritDoc
      * @throws ActionNotAllowedForAdministratorExeption
      * @throws UserBannedException
      */
@@ -21,7 +17,7 @@ class UserService implements UserServiceInterface
     {
         if ($this->canBePerformedOnUser($user)) {
             if (!$this->isBanned($user)) {
-                $user->disable();
+                $user->enabled = false;
                 $user->save();
                 return true;
             } else {
@@ -32,7 +28,6 @@ class UserService implements UserServiceInterface
     }
 
     /**
-     * @inheritDoc
      * @throws UserNotBannedException
      * @throws ActionNotAllowedForAdministratorExeption
      */
@@ -40,7 +35,7 @@ class UserService implements UserServiceInterface
     {
         if ($this->canBePerformedOnUser($user)) {
             if ($this->isBanned($user)) {
-                $user->enable();
+                $user->enabled = true;
                 $user->save();
                 return true;
             } else {
@@ -50,34 +45,15 @@ class UserService implements UserServiceInterface
         return false;
     }
 
-    /**
-     * @inheritDoc
-     */
     function isBanned(User $user): bool
     {
         return !$user->enabled;
     }
 
     /**
-     * @inheritDoc
-     */
-    function getOrders(User $user): Collection
-    {
-        return Order::whereCustomer($user->id)->get();
-    }
-
-    /**
-     * @inheritDoc
-     */
-    function getShoppingCart(User $user): ShoppingCart
-    {
-        return ShoppingCart::whereUserId($user->id);
-    }
-
-    /**
      * @throws ActionNotAllowedForAdministratorExeption
      */
-    private function canBePerformedOnUser(User $user): bool
+    function canBePerformedOnUser(User $user): bool
     {
         if (!$user->isAdmin) {
             return true;
--- app/Services/Users/UserServiceInterface.php
+++ app/Services/Users/UserServiceInterface.php
@@ -2,43 +2,35 @@
 
 namespace App\Services\Users;
 
-use App\Models\ShoppingCart;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Collection;
 
-interface UserServiceInterface {
+interface UserServiceInterface
+{
     /**
      * Bans a specific User
      * @param User $user the specific User Model
      * @return bool true if the User has been banned successfully else false
      */
-    function ban(User $user) : bool;
+    function ban(User $user): bool;
 
     /**
      * Unbans the specific User
      * @param User $user the specific User Model
      * @return bool true if the User has been unbanned else false
      */
-    function unban(User $user) : bool;
+    function unban(User $user): bool;
 
     /**
      * Returns if the User is banned or not
      * @param User $user the specific User Model
      * @return bool true if the User is banned else false
      */
-    function isBanned(User $user) : bool;
+    function isBanned(User $user): bool;
 
     /**
-     * Returns the Users Orders
-     * @param User $user the specific User Model
-     * @return Collection the Collection containing the Users Orders
+     * Determines if an Action can be performed on a given User.
+     * @param User $user The user which should be checked for.
+     * @return bool true if the action can be performed. else false.
      */
-    function getOrders(User $user) : Collection;
-
-    /**
-     * Returns the Shopping Cart of a specific User
-     * @param User $user the specific User Model
-     * @return ShoppingCart the Shopping Cart Model
-     */
-    function getShoppingCart(User $user) : ShoppingCart;
+    function canBePerformedOnUser(User $user): bool;
 }
