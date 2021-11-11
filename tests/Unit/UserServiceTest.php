05c00b0f50bac540f2914898841822817d9d05ed
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-11 13:36:26 +0100

    fixed test cases :)
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- tests/Unit/UserServiceTest.php
+++ tests/Unit/UserServiceTest.php
@@ -1,57 +1,63 @@
 <?php
 
 use App\Exceptions\ActionNotAllowedForAdministratorExeption;
+use App\Models\Admin;
 use App\Models\User;
 use App\Services\Users\UserService;
-use function PHPUnit\Framework\assertFalse;
-use function PHPUnit\Framework\assertTrue;
 
 test('user can be banned', function () {
-    $service = app()->make(UserService::class);
+    $this->actingAs(Admin::all()->random());
+    $service = $this->app->make(UserService::class);
     $user = User::whereEnabled(true)->where('isAdmin', false)->get()->random();
     User::factory()->create();
     $service->ban($user);
-    assertFalse(User::whereId($user->id)->first()->enabled);
+    expect(User::whereId($user->id)->first()->enabled)->toBeFalse();
 });
 
 test('user can be unbanned', function () {
-    $service = app()->make(UserService::class);
+    $this->actingAs(Admin::all()->random());
+    $service = $this->app->make(UserService::class);
     $user = User::where('isAdmin', '=', false)->get()->random();
     User::factory()->create();
     $user->enabled = false;
     $user->save();
     $service->unban($user);
-    assertTrue(User::whereId($user->id)->first()->enabled);
+    expect(User::whereId($user->id)->first()->enabled)->toBeTrue();
 });
 
 test('admin can not be banned', function () {
-    $service = app()->make(UserService::class);
-    $admin = User::whereEnabled(true)->where('isAdmin', true)->get()->random();
+    $this->actingAs(Admin::all()->random());
+    $service = $this->app->make(UserService::class);
+    $admin = Admin::whereEnabled(true)->get()->random();
 
     $service->ban($admin);
 })->throws(ActionNotAllowedForAdministratorExeption::class);
 
 test('admin can not be unbanned', function () {
-    $service = app()->make(UserService::class);
-    $admin = User::whereEnabled(true)->where('isAdmin', true)->get()->random();
+    $this->actingAs(Admin::all()->random());
+    $service = $this->app->make(UserService::class);
+    $admin = Admin::all()->random();
 
     $admin->enabled = false;
     $admin->save();
     $service->unban($admin);
 })->throws(ActionNotAllowedForAdministratorExeption::class);
 
-test('is user banned', function ()  {
-    $service = app()->make(UserService::class);
-    $user = User::where('isAdmin', '=', false)->get()->random();
-    User::factory()->create();
+test('is user banned', function () {
+    $this->actingAs(Admin::all()->random());
+    $service = $this->app->make(UserService::class);
+    $user = User::all()->random();
     $user->enabled = false;
     $user->save();
-    assertTrue($service->isBanned($user));
+    expect($service->isBanned($user))->toBeTrue();
 });
 
-test('is user not banned', function ()  {
-    $service = app()->make(UserService::class);
+test('is user not banned', function () {
+    $this->actingAs(Admin::all()->random());
+    $service = $this->app->make(UserService::class);
     $user = User::whereEnabled(true)->get()->random();
 
-    assertFalse($service->isBanned($user));
+    expect($service->isBanned($user))->toBeFalse();
 });
+
+//TODO make test if disabled_by property is set correct throughout events. Events need to be listening on bans but idk how
