daf3c52b03ee914a828d08ac26f7571dd75b79d2
Jan Franz Kammellander <jkammellander@student.tgm.ac.at>
2021-11-11 20:13:40 +0100

    Frontend base
    beautiful
    shut the fuck up
    have a great day

--- resources/ts/bootstrap.ts
+++ resources/ts/bootstrap.ts
@@ -1,4 +1,4 @@
-window._ = require('lodash');
+(window as any)._ = require('lodash');
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -6,9 +6,9 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+(window as any).axios = require('axios');
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+(window as any).axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -18,9 +18,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import Echo from 'laravel-echo';
 
-window.Pusher = require('pusher-js');
+(window as any).Pusher = require('pusher-js');
 
-window.Echo = new Echo({
+(window as any).Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
