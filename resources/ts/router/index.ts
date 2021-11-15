525566d1a84872a4eaa4bf69195b8dd0917c6267
Jan Franz Kammellander <jkammellander@student.tgm.ac.at>
2021-11-15 16:52:23 +0100

    deleted the About page

--- resources/ts/router/index.ts
+++ resources/ts/router/index.ts
@@ -8,17 +8,11 @@ const routes: Array<RouteRecordRaw> = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/login",
+    name: "Login",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/login",
-    name: "Login",
     component: () =>
       import("../views/Login.vue"),
   },
