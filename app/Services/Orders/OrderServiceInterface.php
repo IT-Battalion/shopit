7d7b58fdae8b30f2074684d6a1a0c615c0e21016
pelias006 <pelias@student.tgm.ac.at>
2021-11-15 10:12:52 +0100

    basic functions

--- app/Services/Orders/OrderServiceInterface.php
+++ app/Services/Orders/OrderServiceInterface.php
@@ -5,16 +5,11 @@ namespace App\Services\Orders;
 // TODO: implement a service that helps handling orders
 // TODO: not here but Tests for every relation between models to verify that they work
 // TODO: add default constructors to every model class
-// TODO: why is this in that branch????
 interface OrderServiceInterface
 {
-<<<<<<< HEAD
     public function createOrder();
 
     public function deleteOrder();
 
     public function getOrder();
-=======
-
->>>>>>> :construction: :construction: :construction: Small changes
 }
