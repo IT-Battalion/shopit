fbe3ad57ff221fd90b7e441ba850d20ceca497a9
Jan Franz Kammellander <jkammellander@student.tgm.ac.at>
2021-11-15 16:50:51 +0100

    Login page added

---
+++ resources/ts/views/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <!-- the submit event will no longer reload the page -->
+  <form v-on:submit.prevent="submitForm">
+    <h1>Login</h1>
+    <div class="form-control">
+      <label for="user-name">Benutzername</label>
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model="userName"
+        ref="userName"
+        v-on:keyup.enter="submitForm"
+      />
+      <label for="user-password">Passwort</label>
+      <input
+        id="user-password"
+        name="user-name"
+        type="password"
+        v-model="userPassword"
+        ref="userPassword"
+        v-on:keyup.enter="submitForm"
+      />
+      <!-- only call `submitForm()` when the `key` is `Enter` -->
+    </div>
+
+    <div>
+      <button>Anmelden</button>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      userName: "",
+      userPassword: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log(this.$refs.userName.value + " | " + this.$refs.userPassword.value); // one get input value
+    },
+  },
+};
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                backgroundColor: "#1E1D2B",
+            }
+        },
     },
     variants: {
         extend: {},
