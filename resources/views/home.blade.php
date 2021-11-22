5100ab328db233898440d61484ff9b11dad037ac
pelias006 <47031872+pelias006@users.noreply.github.com>
2021-11-22 13:45:07 +0100

    retrieving categories in frontend attempt
    Signed-off-by: pelias006 <47031872+pelias006@users.noreply.github.com>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,6 @@
 @extends('layouts.app')
+@section('head-content')
+    <script>
+        window.config.categories = {{ json_encode(\App\Models\ProductCategory::all()->toArray()) }}
+    </script>
+@endsection
