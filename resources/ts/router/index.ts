76352eca2c1500a550d2ba79975e459a1e4840d4
Jan Franz Kammellander <jkammellander@student.tgm.ac.at>
2021-11-24 15:09:15 +0100

    added the main product page 👕

--- resources/ts/router/index.ts
+++ resources/ts/router/index.ts
@@ -16,6 +16,15 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import("../views/Login.vue"),
   },
+  {
+    path: "/products",
+    name: "Products",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import("../views/Products.vue"),
+  },
 ];
 
 const router = createRouter({
