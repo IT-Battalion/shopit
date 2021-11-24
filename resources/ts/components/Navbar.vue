5657127120d0260a72d337cbc27b3834771f1ee4
Jan Franz Kammellander <jkammellander@student.tgm.ac.at>
2021-11-24 15:10:12 +0100

    trying to make the navbar 😭

---
+++ resources/ts/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+        <ul>
+            <li>Logo</li>
+            <li><img :src="profilePicture" />{{ userName }}</li>
+            <li><img :src="iconShoppingCart" /></li>
+        </ul>
+    </nav>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            profilePicture: "test",
+            userName: "test",
+            iconShoppingCart: "test",
+        };
+    },
+};
+</script>
