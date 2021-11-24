5af3281620bb549130335392c579fc5c565ba511
Jan Franz Kammellander <jkammellander@student.tgm.ac.at>
2021-11-24 15:09:55 +0100

    added the category sidenav on the product page 👀

--- resources/ts/components/sidenav.vue
+++ resources/ts/components/sidenav.vue
@@ -1,7 +1,28 @@
-<template></template>
+<template>
+    <div>
+        <ul
+            class="fixed left-0 flex flex-col justify-center w-40 h-screen m-0 text-white shadow-lg  bg-backgroundColor top-10"
+        >
+            <a
+                v-for="category in categories"
+                v-bind:key="category.icon_url"
+                v-bind:name="category.name"
+                class="flex flex-row my-6 ml-6"
+                ><img :src="category.icon_url" class="mr-4" />
+                {{ category.name }}</a
+            >
+        </ul>
+    </div>
+</template>
 
-<script>
-export default {};
+<script lang="ts">
+export default {
+    data() {
+        return {
+            categories: window.config.categories,
+        };
+    },
+};
 </script>
 
 <style></style>
