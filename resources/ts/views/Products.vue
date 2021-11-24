638072bef2d93c0bdfa9d8b06ee12f2202cd0754
Jan Franz Kammellander <jkammellander@student.tgm.ac.at>
2021-11-24 15:09:15 +0100

    added the main product page 👕

---
+++ resources/ts/views/Products.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="w-screen h-screen bg-backgroundColor">
+        <div class="flex flex-row m-auto">
+            <Sidenav></Sidenav>
+            <div class="m-auto bg-gray-100"><p>Hilfe</p></div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Sidenav from "../components/Sidenav.vue";
+import Navbar from "../components/Navbar.vue";
+export default {
+    components: {
+        Sidenav,
+        Navbar,
+    },
+};
+</script>
