dd2bd28c3f57e1de79cab8e4fbf00006956aa9ab
Jan Franz Kammellander <jkammellander@student.tgm.ac.at>
2021-11-25 08:06:52 +0100

    Login fully works now on from my frontend perspective 🤡

--- resources/ts/App.vue
+++ resources/ts/App.vue
@@ -1,7 +1,6 @@
 <template>
     <div id="nav">
         <router-link to="/">Home</router-link> |
-        <router-link to="/login">Login</router-link> |
         <router-link to="/products">Products</router-link>
     </div>
     <router-view />
--- resources/ts/router/index.ts
+++ resources/ts/router/index.ts
@@ -1,20 +1,12 @@
 import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
-import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Products from "../views/Products.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/login",
     name: "Login",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import("../views/Login.vue"),
+    component: Login,
   },
   {
     path: "/products",
