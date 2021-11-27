16de251716d36be07d731eb772376f118cc8d69a
pdamianik <39028343+pdamianik@users.noreply.github.com>
2021-11-28 00:33:06 +0100

    :construction: Got Echo to work
    Websockets, oh what great future they forebode.
    Also, a reminder: Sail is just a temporary development environment and not a replacement for a real production docker setup (because of issues like running off of the php development server or having the redis container exposed to the external network of the host machine (which may be useful in development but could definitely cause problems, especially some security issues, when used in a production environment)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,6 +44,7 @@ class Kernel extends HttpKernel
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'auth:sanctum',
             'throttle:api',
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,7 +39,7 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
                 'host' => '127.0.0.1',
-                'port' => 6001,
+                'port' => env('PUSHER_APP_PORT'),
                 'scheme' => 'http',
                 //'useTLS' => true,
             ],
