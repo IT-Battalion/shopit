55bdeb90fc894b8b7d1148b0976eb4b2612ea7c7
pdamianik <39028343+pdamianik@users.noreply.github.com>
2021-11-28 01:11:07 +0100

    :construction: Some Translations

--- resources/lang/de/license.php
+++ resources/lang/de/license.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'public-domain' => 'This icon is in the public domain',
-    'creative-commons-by-30' => '<a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="https://i.creativecommons.org/l/by/3.0/88x31.png" /></a><br />Dieses Icon ist lizenziert unter einer <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Namensnennung 3.0 Unported Lizenz</a>.'
+    'public-domain' => 'Dieses Symbol ist lizenzfrei',
+    'creative-commons-by-30' => '<a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="https://i.creativecommons.org/l/by/3.0/88x31.png" /></a><br />Dieses Werk ist lizenziert unter einer <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Namensnennung 3.0 Unported Lizenz</a>.'
 ];
