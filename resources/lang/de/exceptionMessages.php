4f4ed08f26f5924d0925b8784a7ade7dd01b3525
pdamianik <39028343+pdamianik@users.noreply.github.com>
2021-11-28 01:11:07 +0100

    :construction: Some Translations

---
+++ resources/lang/de/exceptionMessages.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'insufficient_permission' => 'Sie haben nicht genügend Rechte für diese Aktion.',
+    'user_not_banned' => 'Dieser Nutzer ist aktuell nicht gebannt.',
+    'user_already_banned' => 'Dieser Nutzer ist bereits gebannt.',
+    'action_not_allowed_on_administrator' => 'Diese Aktion kann nicht an einem Administrator durchgeführt werden.',
+    'product_not_found_for_deletion' => 'Das Produkt kann nicht gelöscht werden, da es nicht gefunden wurde.',
+    'product_not_found_for_image' => 'Ein Bild kann nicht einem fehlenden Produkt hinzugefügt werden.',
+    'product_not_found_for_attribute' => 'Ein Attribut kann nicht einem fehlenden Produkt hinzugefügt werden.',
+    'product_image_not_found_for_thumbnail' => 'Ein fehlendes Bild kann nicht das Thumbnail eines Produkts werden.',
+    'category_not_found' => 'Die Kategorie konnte nicht gefunden werden.',
+    'product_not_found' => 'Das Produkt konnte nicht gefunden werden.',
+    'coupon_code_discount_out_of_bounds' => 'Der Rabatt des Gutscheins muss zwischen 1 und 100 liegen.',
+    'coupon_code_not_found_exception' => 'Es konnte kein Gutschein mit ihrem Gutschein Code gefunden werden.',
+    'highlighted_product_not_found' => 'Das Produkt konnte nicht gefunden werden.',
+    'illegal_amount_given' => 'Die Anzahl ist nicht erlaubt.',
+    'product_not_in_shopping_cart' => 'Das Produkt ist nicht im Einkaufswagen.',
+    'order_not_paid' => 'Die Bestellung wurde noch nicht bezahlt.',
+    'order_not_ordered' => 'Die Bestellung wurde noch nicht von einem Administrator bestellt.',
+    'order_not_received' => 'Die Bestellung wurde noch nicht von einem Administrator erhalten.',
+    'shopping_cart_empty' => 'Eine Bestellung kann nicht von einem leeren Einkaufswagen zusammengestellt werden.',
+];
---
+++ resources/lang/en/license.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'public-domain' => 'This icon is in the public domain',
+    'creative-commons-by-30' => '<a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/3.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a>.'
+];
