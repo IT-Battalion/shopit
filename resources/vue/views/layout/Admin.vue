7b598c836903edd6fdf53164c4c2389620bcdf27
Jan Kammellander <janschwiderik@gmail.com>
2022-01-05 01:46:38 +0100

    added a new layout for the admin pages

---
+++ resources/vue/views/layout/Admin.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-row">
+    <Sidenav />
+    <div
+      class="
+        flex flex-col
+        items-center
+        justify-center
+        w-full
+        h-screen
+        md:h-full md:w-11/12
+        mt-14
+      "
+    >
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import SidenavAdmin from "@/components/SidenavAdmin.vue";
+export default {
+  components: {
+    SidenavAdmin,
+  },
+};
+</script>
