5ee89590ba846ec0569347469ffe7e86afa37b19
Jan Kammellander <janschwiderik@gmail.com>
2022-01-05 01:13:00 +0100

    added the first admin page components 🎊

---
+++ resources/vue/components/SidenavAdmin.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    class="
+      fixed
+      left-0
+      flex flex-col
+      justify-center
+      invisible
+      w-56
+      pl-4
+      m-0
+      text-white
+      md:visible
+      sidebar
+      top-32
+      bg-backgroundColor
+    "
+  >
+    <div
+      v-for="category in categories"
+      v-bind:key="category.icon_url"
+      v-bind:name="category.name"
+      class="flex flex-row my-6 ml-6"
+    >
+      <img :src="category.icon_url" class="object-scale-down h-8 mr-4" />
+      <a class="my-1 text-left" :href="'#' + category.name">{{
+        category.name
+      }}</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  setup() {
+    const categories = [
+      { name: "Meine Daten", icon_url: "#" },
+      { name: "Rechnungen", icon_url: "#" },
+      { name: "Bestellverlauf", icon_url: "#" },
+      { name: "Produkte", icon_url: "#" },
+      { name: "Kategorien", icon_url: "#" },
+      { name: "Bestellungen", icon_url: "#" },
+      { name: "Userverwaltung", icon_url: "#" },
+      { name: "Coupons", icon_url: "#" },
+    ];
+    return { categories };
+  },
+};
+</script>
+
+<style>
+.sidebar {
+  height: calc(100% - 8rem);
+}
+</style>
---
+++ resources/vue/components/profilePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img
+      :src="profilePicture"
+      class="object-scale-down h-12 mr-4 bg-gray-800 rounded-full"
+    />
+    <div>
+      <h1>{{ user.name.value }}</h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+import useUser from "../stores/user";
+
+export default defineComponent({
+  setup() {
+    const { user, logout } = useUser();
+    const profilePicture =
+      "https://avatars.dicebear.com/api/micah/:" + user.username.value + ".svg";
+    console.log(user);
+
+    return { user, logout, profilePicture };
+  },
+});
+</script>
