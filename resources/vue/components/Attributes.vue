5a53988b736d9d1b21009418917d227d25b6a020
Jan Franz Kammellander <janschwiderik@gmail.com>
2022-01-20 16:02:46 +0100

    attributes are now displayed in the shopping cart entries 💅

--- resources/vue/components/Attributes.vue
+++ resources/vue/components/Attributes.vue
@@ -1,23 +1,34 @@
 <template>
-  <div :class="[this.class]">
-    <div v-if="attributes['0']">
-
-    </div>
-    <div v-if="attributes['1']">
-
+  <div
+    :class="[this.class]"
+    class="grid grid-cols-1 text-xs text-gray-400 grid-rows-"
+  >
+    <div v-if="attributes['0']">Kleidergröße: {{ attributes[0].id }}</div>
+    <div
+      v-if="attributes['1']"
+      class="grid grid-cols-2 grid-rows-3 place-items-center"
+    >
+      <a class="row-span-3 cols-span-1">Dimension: </a
+      ><a class="row-span-1 cols-span-1"
+        >{{ attributes[1].width.value }}{{ attributes[1].width.unit }} x</a
+      >
+      <a class="row-span-1 cols-span-1"
+        >{{ attributes[1].height.value }}{{ attributes[1].height.unit }} x</a
+      >
+      <a class="row-span-1 cols-span-1"
+        >{{ attributes[1].depth.value }}{{ attributes[1].depth.unit }}</a
+      >
     </div>
     <div v-if="attributes['2']">
-
+      Volumen: {{ attributes[2].volume.value }}{{ attributes[2].volume.unit }}
     </div>
-    <div v-if="attributes['3']">
-
-    </div>
+    <div v-if="attributes['3']">Farbe: {{ attributes[3].name }}</div>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent, PropType} from "vue";
-import {SelectedAttributes} from "../types/api";
+import { defineComponent, PropType } from "vue";
+import { SelectedAttributes } from "../types/api";
 
 export default defineComponent({
   props: {
@@ -27,7 +38,8 @@ export default defineComponent({
     },
     class: {
       type: String,
-    }
+      default: "",
+    },
   },
   data() {
     return {
