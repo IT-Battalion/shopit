e8ea0be96e9dda2a0dd45bf599b1c3e3c7e92d11
pelias <pelias@student.tgm.ac.at>
2022-01-23 14:46:42 +0100

    Add: Contributors Page first Version
    Signed-off-by: pelias <pelias@student.tgm.ac.at>

--- resources/vue/components/Contributors.vue
+++ resources/vue/components/Contributors.vue
@@ -1,22 +1,51 @@
 <template>
   <h1 class="text-4xl font-bold text-white">Contributors of this Project</h1>
+  <br>
+  <h1 class="text-3xl font-bold text-white mt-2">Special thanks to: </h1>
+  <br>
+  <h2 class="text-1xl font-bold text-white">Stefan Zakall</h2>
+  <p class="text-white">Für das Ausarbeiten der Idee, sowie die Begleitung während der Umsetzung.</p>
+  <br>
+  <h2 class="text-1xl font-bold text-white">Michael Weber</h2>
+  <p class="text-white">Für die Hilfe bei der LDAP Authentifizierung ;)</p>
+  <br>
+  <h2 class="text-1xl font-bold text-white">Christoph Roschger</h2>
+  <p class="text-white">Für die Hilfe und Unterstützung bei Sicherheitsfragen sowie das versenden von Mails.</p>
+  <br>
+  <h2 class="text-1xl font-bold text-white">Walter Raffainer-Magor</h2>
+  <p class="text-white">Für das Überprüfen und bestätigen diverser Dokumente wie Rechnungen, ..</p>
+  <br>
+  <h2 class="text-1xl font-bold text-white">Eva-Marie Kriechbaum</h2>
+  <p class="text-white">Für die Unterstützung bei der Klärung rechtlicher Fragen.</p>
+  <br>
+  <h2 class="text-1xl font-bold text-white">Monika Reichart</h2>
+  <p class="text-white">Für viel Inspiration und Motivation.</p>
+  <br>
+  <h2 class="text-1xl font-bold text-white">RamsesTheTeacher</h2>
+  <p class="text-white">Für Unterhaltsame Begleitung während schlafloser Nächte.</p>
+  <br>
 
-  Stefan Zakall - Für das Ausarbeiten der Idee, sowie die Begleitung während der Umsetzung.
-  Michael Weber - Für die Hilfe bei der LDAP Authentifizierung ;)
-  Christoph Roschger - Für die Hilfe und Unterstützung bei Sicherheitsfragen sowie das versenden von Mails.
-  Walter Raffainer-Magor - Für das Überprüfen und bestätigen diverser Dokumente wie Rechnungen, ..
-  Monika Reichart - Für viel Inspiration und Motivation.
-  RamsesTheTeacher - Für Unterhaltsame Begleitung während schlafloser Nächte.
 
-
-  Patrick Elias
-  Projektleiter und Full-Stack Developer
-
-  Philip Damianik
-  Full-Stack Developer und Systemadministrator
-
-  Jan Kammellander
-  Frontend Developer und Grafik Designer
+  <div class="mt-20">
+    <h2 class="text-3xl text-white font-bold text-center">Developer Team</h2>
+    <div class="flex flex-row">
+      <div class="flex flex-col w-1/3 text-center">
+        <img alt="pelias" src="/img/pelias.png">
+        <span class="text-white text-2xl font-bold">Patrick Elias</span>
+        <span class="text-gray-200">Projektleiter & Full-Stack Developer</span>
+      </div>
+      <div class="flex flex-col w-1/3 text-center">
+        <img alt="pdamianik" src="/img/pdamianik.png">
+        <span class="text-white text-2xl font-bold">Philip Damianik</span>
+        <span class="text-gray-200">Full-Stack Developer & Systemadministrator</span>
+      </div>
+      <div class="flex w-1/3 flex-col text-center">
+        <img alt="jkammellander" src="/img/jkammellander.png">
+        <span class="text-white text-2xl font-bold">Jan Kammellander</span>
+        <span class="text-gray-200">Frontend Developer & Grafik Designer</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
