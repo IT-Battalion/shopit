9eecc5d1801388df481a6bc6719bc8d2d2ff1675
pelias <pelias@student.tgm.ac.at>
2022-01-23 14:49:24 +0100

    Improve: Color of Contributors
    Signed-off-by: pelias <pelias@student.tgm.ac.at>

--- resources/vue/components/Contributors.vue
+++ resources/vue/components/Contributors.vue
@@ -1,28 +1,28 @@
 <template>
   <h1 class="text-4xl font-bold text-white">Contributors of this Project</h1>
   <br>
-  <h1 class="text-3xl font-bold text-white mt-2">Special thanks to: </h1>
+  <h1 class="text-3xl font-bold text-white mt-2">Special thanks goes to: </h1>
   <br>
   <h2 class="text-1xl font-bold text-white">Stefan Zakall</h2>
-  <p class="text-white">Für das Ausarbeiten der Idee, sowie die Begleitung während der Umsetzung.</p>
+  <p class="text-gray-200">Für das Ausarbeiten der Idee, sowie die Begleitung während der Umsetzung.</p>
   <br>
   <h2 class="text-1xl font-bold text-white">Michael Weber</h2>
-  <p class="text-white">Für die Hilfe bei der LDAP Authentifizierung ;)</p>
+  <p class="text-gray-200">Für die Hilfe bei der LDAP Authentifizierung ;)</p>
   <br>
   <h2 class="text-1xl font-bold text-white">Christoph Roschger</h2>
-  <p class="text-white">Für die Hilfe und Unterstützung bei Sicherheitsfragen sowie das versenden von Mails.</p>
+  <p class="text-gray-200">Für die Hilfe und Unterstützung bei Sicherheitsfragen sowie das versenden von Mails.</p>
   <br>
   <h2 class="text-1xl font-bold text-white">Walter Raffainer-Magor</h2>
-  <p class="text-white">Für das Überprüfen und bestätigen diverser Dokumente wie Rechnungen, ..</p>
+  <p class="text-gray-200">Für das Überprüfen und bestätigen diverser Dokumente wie Rechnungen, ..</p>
   <br>
   <h2 class="text-1xl font-bold text-white">Eva-Marie Kriechbaum</h2>
-  <p class="text-white">Für die Unterstützung bei der Klärung rechtlicher Fragen.</p>
+  <p class="text-gray-200">Für die Unterstützung bei der Klärung rechtlicher Fragen.</p>
   <br>
   <h2 class="text-1xl font-bold text-white">Monika Reichart</h2>
-  <p class="text-white">Für viel Inspiration und Motivation.</p>
+  <p class="text-gray-200">Für viel Inspiration und Motivation.</p>
   <br>
   <h2 class="text-1xl font-bold text-white">RamsesTheTeacher</h2>
-  <p class="text-white">Für Unterhaltsame Begleitung während schlafloser Nächte.</p>
+  <p class="text-gray-200">Für Unterhaltsame Begleitung während schlafloser Nächte.</p>
   <br>
 
 
