ccb14acb62e3219a9354331de7182bc3f9087764
pelias <pelias@student.tgm.ac.at>
2022-01-23 14:07:15 +0100

    Add: Basic Text for Contributors
    Signed-off-by: pelias <pelias@student.tgm.ac.at>

--- resources/vue/components/Contributors.vue
+++ resources/vue/components/Contributors.vue
@@ -1,7 +1,22 @@
 <template>
-  <h1>Contributors of this Project</h1>
+  <h1 class="text-4xl font-bold text-white">Contributors of this Project</h1>
 
-  <!-- tba (ultra nice to have)-->
+  Stefan Zakall - Für das Ausarbeiten der Idee, sowie die Begleitung während der Umsetzung.
+  Michael Weber - Für die Hilfe bei der LDAP Authentifizierung ;)
+  Christoph Roschger - Für die Hilfe und Unterstützung bei Sicherheitsfragen sowie das versenden von Mails.
+  Walter Raffainer-Magor - Für das Überprüfen und bestätigen diverser Dokumente wie Rechnungen, ..
+  Monika Reichart - Für viel Inspiration und Motivation.
+  RamsesTheTeacher - Für Unterhaltsame Begleitung während schlafloser Nächte.
+
+
+  Patrick Elias
+  Projektleiter und Full-Stack Developer
+
+  Philip Damianik
+  Full-Stack Developer und Systemadministrator
+
+  Jan Kammellander
+  Frontend Developer und Grafik Designer
 </template>
 
 <script lang="ts">
