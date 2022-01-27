567412993fce12807d602abd4b91bd1f3fd0a361
pelias006 <pelias@student.tgm.ac.at>
2022-01-27 19:31:39 +0100

    Add: checkbox for highlighted products

--- resources/vue/views/AddProductMeta.vue
+++ resources/vue/views/AddProductMeta.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
-    <AddProductProcess />
+    <AddProductProcess/>
     <div class="grid w-full grid-cols-1 grid-rows-2 my-16 place-items-center">
-      <InputField labelName="Produktname" />
-      <InputField labelName="Preis" type="number" />
+      <InputField labelName="Produktname"/>
+      <InputField labelName="Preis" type="number"/>
+      <div class="flex flex-row my-5">
+        <input
+          type="checkbox"
+          name="highlighted"
+          id="highlighted"
+          class="my-5"
+        />
+        <label for="highlighted" class="my-auto ml-2 text-center text-white">
+          Produkt hervorheben.
+        </label>
+      </div>
     </div>
     <ForwardBackwardButton
       :next="{ name: 'Add Product images' }"
@@ -14,7 +25,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@vue/runtime-core";
+import {defineComponent} from "@vue/runtime-core";
 import AddProductProcess from "../components/AddProductProcess.vue";
 import ForwardBackwardButton from "../components/ForwardBackwardButton.vue";
 import InputField from "../components/InputField.vue";
