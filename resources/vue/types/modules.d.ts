17932f9278fbb2d3cf6fdc662dcd7c8da2b10f40
Jan Kammellander <janschwiderik@gmail.com>
2022-01-29 18:36:14 +0100

    added a new file dropzone librabry ⚙️

--- resources/vue/types/modules.d.ts
+++ resources/vue/types/modules.d.ts
@@ -1,3 +1,2 @@
 declare module 'vue3-mq';
-declare module 'vue-upload-drop-images';
-declare module 'vuesax';
+declare module 'vue - filepond';
--- resources/vue/views/UploadProductImages.vue
+++ resources/vue/views/UploadProductImages.vue
@@ -1,37 +1,62 @@
 <template>
   <div>
-    <AddProductProcess/>
+    <AddProductProcess />
     <div class="mx-5 mt-20 md:mx-20">
-      <UploadImages ref="images"/>
-      <!--      <ButtonField-->
-      <!--        name="Hochladen"-->
-      <!--        iconSrc="/img/uploadBlack.svg"-->
-      <!--        class="my-10 mx-auto"-->
-      <!--      />-->
+      <file-pond
+        name="test"
+        ref="pond"
+        label-idle="Drop files here..."
+        v-bind:allow-multiple="true"
+        accepted-file-types="image/jpeg, image/png"
+        server="/api"
+        v-bind:files="myFiles"
+        v-on:init="handleFilePondInit"
+      />
     </div>
     <div class="flex justify-end mt-10 sm:mr-20">
-      <CancelButton/>
-      <BackwardButton @click="backward"/>
-      <ForwardButton @click="forward"/>
+      <CancelButton />
+      <BackwardButton />
+      <ForwardButton />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "@vue/runtime-core";
-import UploadImages from "vue-upload-drop-images";
+import { defineComponent } from "@vue/runtime-core";
 import AddProductProcess from "../components/AddProductProcess.vue";
 import ButtonField from "../components/ButtonField.vue";
 import CancelButton from "../components/buttons/CancelButton.vue";
 import BackwardButton from "../components/buttons/BackwardButton.vue";
 import ForwardButton from "../components/buttons/ForwardButton.vue";
-import {ProductCreateProcessStorage} from "../types/api";
-import {isUndefined} from "lodash";
+import { ProductCreateProcessStorage } from "../types/api";
+import { isUndefined } from "lodash";
+
+// Import Vue FilePond
+import vueFilePond from "vue-filepond";
+
+// Import FilePond styles
+import "filepond/dist/filepond.min.css";
+
+// Import FilePond plugins
+// Please note that you need to install these plugins separately
+
+// Import image preview plugin styles
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
+
+// Import image preview and file type validation plugins
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+
+// Create component
+const FilePond = vueFilePond(
+  FilePondPluginFileValidateType,
+  FilePondPluginImagePreview
+);
 
 export default defineComponent({
   components: {
     BackwardButton,
-    UploadImages,
+    FilePond,
     AddProductProcess,
     ButtonField,
     ForwardButton,
@@ -40,35 +65,50 @@ export default defineComponent({
   data() {
     return {
       productCreateStorage: Object as ProductCreateProcessStorage,
+      myFiles: ["cat.jpeg"],
     };
   },
-  async mounted() {
-    let data = window.localStorage.getItem('product');
-    if (!isUndefined(data) && data != null && data != 'undefined') {
-      this.productCreateStorage = JSON.parse(data);
-    }
-    await this.insertStoredData();
-  },
+  // async mounted() {
+  //   let data = window.localStorage.getItem("product");
+  //   if (!isUndefined(data) && data != null && data != "undefined") {
+  //     this.productCreateStorage = JSON.parse(data);
+  //   }
+  //   await this.insertStoredData();
+  // },
   methods: {
-    async insertStoredData() {
-      if (!isUndefined(this.productCreateStorage) && !isUndefined(this.productCreateStorage.images)) {
-        if (this.productCreateStorage.images.length > 0) {
-          (this.$refs.images as typeof UploadImages).Imgs = this.productCreateStorage.images;
-        }
-      }
+    // async insertStoredData() {
+    //   if (
+    //     !isUndefined(this.productCreateStorage) &&
+    //     !isUndefined(this.productCreateStorage.images)
+    //   ) {
+    //     if (this.productCreateStorage.images.length > 0) {
+    //       (this.$refs.images as typeof UploadImages).Imgs =
+    //         this.productCreateStorage.images;
+    //     }
+    //   }
+    // },
+    // async backward() {
+    //   // await this.saveToLocalStorage();
+    //   await this.$router.push({ name: "Add Product" });
+    // },
+    // async forward() {
+    //   // await this.saveToLocalStorage();
+    //   await this.$router.push({ name: "Add Product attributes" });
+    // },
+    // async saveToLocalStorage() {
+    //   this.productCreateStorage.images = (
+    //     this.$refs.images as typeof UploadImages
+    //   ).Imgs;
+    //   window.localStorage.setItem(
+    //     "product",
+    //     JSON.stringify(this.productCreateStorage)
+    //   );
+    // },
+    handleFilePondInit: function () {
+      console.log("FilePond has initialized");
+
+      // FilePond instance methods are available on `this.$refs.pond`
     },
-    async backward() {
-      await this.saveToLocalStorage();
-      await this.$router.push({name: 'Add Product'});
-    },
-    async forward() {
-      await this.saveToLocalStorage();
-      await this.$router.push({name: 'Add Product attributes'});
-    },
-    async saveToLocalStorage() {
-      this.productCreateStorage.images = (this.$refs.images as typeof UploadImages).Imgs;
-      window.localStorage.setItem('product', JSON.stringify(this.productCreateStorage));
-    }
   },
 });
 </script>
