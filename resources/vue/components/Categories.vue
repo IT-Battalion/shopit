fb63e5117e3bef0e9a8d20f8f20991461bc72cb3
Jan Kammellander <janschwiderik@gmail.com>
2022-01-29 19:50:18 +0100

    fixed the catagories background on mobile 🔨

--- resources/vue/components/Categories.vue
+++ resources/vue/components/Categories.vue
@@ -18,12 +18,7 @@
             leave-to="opacity-0"
           >
             <DialogOverlay
-              class="
-                absolute
-                inset-0
-                transition-opacity
-                bg-gray-500 bg-opacity-75
-              "
+              class="absolute inset-0 transition-opacity bg-black bg-opacity-65"
             />
           </TransitionChild>
 
@@ -39,54 +34,21 @@
             >
               <div class="w-screen max-w-md">
                 <div
-                  class="
-                    flex flex-col
-                    h-full
-                    overflow-y-scroll
-                    bg-white
-                    shadow-xl
-                  "
+                  class="flex flex-col h-full overflow-y-scroll shadow-xl  bg-backgroundColor"
                 >
                   <div
-                    class="
-                      flex-1
-                      px-4
-                      py-6
-                      overflow-y-auto
-                      sm:px-6
-                      flex
-                      items-center
-                      justify-center
-                    "
+                    class="flex items-center justify-center flex-1 px-4 py-6 overflow-y-auto  sm:px-6"
                   >
                     <button
                       type="button"
-                      class="
-                        p-2
-                        -m-2
-                        text-gray-400
-                        hover:text-gray-500
-                        bottom-5
-                        absolute
-                        z-10
-                      "
+                      class="absolute z-10 p-2 -m-2 text-gray-400  hover:text-gray-500 bottom-5"
                       @click="isOpen = false"
                     >
                       <span class="sr-only">Close panel</span>
                       <XIcon class="w-6 h-6" aria-hidden="true" />
                     </button>
                     <div
-                      class="
-                        overflow-auto
-                        flex flex-col
-                        justify-center
-                        w-56
-                        pl-4
-                        text-black
-                        sidebar
-                        absolute
-                        bottom-0
-                      "
+                      class="absolute bottom-0 flex flex-col justify-center w-56 pl-4 overflow-auto text-white  sidebar"
                     >
                       <div
                         v-for="category in categories"
@@ -94,11 +56,15 @@
                         class="flex flex-row my-6 ml-6 hover:text-gray-500"
                         @click="isOpen = false"
                       >
-                        <span class="object-scale-down h-8 w-8 mr-4 rounded" :style="'background-color:#'+category.color"></span>
+                        <span
+                          class="object-scale-down w-8 h-8 mr-4 rounded"
+                          :style="'background-color:#' + category.color"
+                        ></span>
                         <router-link
                           class="my-1 text-left"
                           :to="'/#' + category.name"
-                          >{{ category.name }}</router-link>
+                          >{{ category.name }}</router-link
+                        >
                       </div>
                     </div>
                   </div>
