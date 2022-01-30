aae16d0521cc7f4cf613de3c00b3a7cb8e68b93b
Jan Kammellander <janschwiderik@gmail.com>
2022-01-30 22:33:25 +0100

    fixed the contributer page on mobile 🔨

--- resources/vue/components/Contributors.vue
+++ resources/vue/components/Contributors.vue
@@ -1,46 +1,61 @@
 <template>
   <h1 class="text-4xl font-bold text-white">Contributors of this Project</h1>
-  <br>
-  <h1 class="text-3xl font-bold text-white mt-2">Special thanks goes to: </h1>
-  <br>
-  <h2 class="text-1xl font-bold text-white">Stefan Zakall</h2>
-  <p class="text-gray-200">Für das Ausarbeiten der Idee, sowie die Begleitung während der Umsetzung.</p>
-  <br>
-  <h2 class="text-1xl font-bold text-white">Michael Weber</h2>
-  <p class="text-gray-200">Für die Hilfe bei der LDAP Authentifizierung ;) der beste Klassenvorstand.</p>
-  <br>
-  <h2 class="text-1xl font-bold text-white">Christoph Roschger</h2>
-  <p class="text-gray-200">Für die Hilfe und Unterstützung bei Sicherheitsfragen sowie das versenden von Mails.</p>
-  <br>
-  <h2 class="text-1xl font-bold text-white">Walter Rafeiner-Magor</h2>
-  <p class="text-gray-200">Für das Überprüfen und bestätigen diverser Dokumente wie Rechnungen, Bestellbestätigungen und
-    einegem mehr.</p>
-  <br>
-  <h2 class="text-1xl font-bold text-white">Eva-Maria Kriechbaum</h2>
-  <p class="text-gray-200">Für die Unterstützung bei der Klärung rechtlicher Fragen wie Rechnungen, Datenschutz und
-    vielem mehr..</p>
-  <br>
-  <h2 class="text-1xl font-bold text-white">RamsesTheTeacher</h2>
-  <p class="text-gray-200">Für Unterhaltsame Begleitung während schlafloser Nächte. (Wir wollen Horror Games!!)</p>
-  <br>
-
+  <br />
+  <h1 class="mt-2 text-3xl font-bold text-white">Special thanks goes to:</h1>
+  <br />
+  <h2 class="font-bold text-white text-1xl">Stefan Zakall</h2>
+  <p class="text-gray-200">
+    Für das Ausarbeiten der Idee, sowie die Begleitung während der Umsetzung.
+  </p>
+  <br />
+  <h2 class="font-bold text-white text-1xl">Michael Weber</h2>
+  <p class="text-gray-200">
+    Für die Hilfe bei der LDAP Authentifizierung ;) der beste Klassenvorstand.
+  </p>
+  <br />
+  <h2 class="font-bold text-white text-1xl">Christoph Roschger</h2>
+  <p class="text-gray-200">
+    Für die Hilfe und Unterstützung bei Sicherheitsfragen sowie das versenden
+    von Mails.
+  </p>
+  <br />
+  <h2 class="font-bold text-white text-1xl">Walter Rafeiner-Magor</h2>
+  <p class="text-gray-200">
+    Für das Überprüfen und bestätigen diverser Dokumente wie Rechnungen,
+    Bestellbestätigungen und einegem mehr.
+  </p>
+  <br />
+  <h2 class="font-bold text-white text-1xl">Eva-Maria Kriechbaum</h2>
+  <p class="text-gray-200">
+    Für die Unterstützung bei der Klärung rechtlicher Fragen wie Rechnungen,
+    Datenschutz und vielem mehr..
+  </p>
+  <br />
+  <h2 class="font-bold text-white text-1xl">RamsesTheTeacher</h2>
+  <p class="text-gray-200">
+    Für Unterhaltsame Begleitung während schlafloser Nächte. (Wir wollen Horror
+    Games!!)
+  </p>
+  <br />
 
   <div class="mt-20">
-    <h2 class="text-3xl text-white font-bold text-center">Developer Team</h2>
-    <div class="flex flex-row">
-      <div class="flex flex-col w-1/3 text-center">
-        <img alt="pelias" src="/img/pelias.png">
-        <span class="text-white text-2xl font-bold">Patrick Elias</span>
+    <h2 class="text-3xl font-bold text-center text-white">Developer Team</h2>
+    <div class="flex flex-col items-center justify-center sm:flex-row">
+      <div class="flex flex-col items-center justify-center w-1/3 text-center">
+        <img alt="pelias" src="/img/pelias.png" />
+        <span class="text-2xl font-bold text-white">Patrick Elias</span>
         <span class="text-gray-200">Projektleiter & Full-Stack Developer</span>
       </div>
-      <div class="flex flex-col w-1/3 text-center">
-        <img alt="pdamianik" src="/img/pdamianik.png">
-        <span class="text-white text-2xl font-bold">Philip Damianik</span>
-        <span class="text-gray-200">Full-Stack Developer & Systemadministrator</span>
+      <div class="flex flex-col items-center justify-center w-1/3 text-center">
+        <img alt="pdamianik" src="/img/pdamianik.png" />
+        <span class="text-2xl font-bold text-white">Philip Damianik</span>
+        <span class="text-gray-200"
+          >Full-Stack Developer & Systemadministrator</span
+        >
       </div>
-      <div class="flex w-1/3 flex-col text-center">
-        <img alt="jkammellander" src="/img/jkammellander.png">
-        <span class="text-white text-2xl font-bold">Jan Kammellander</span>
+      <div class="flex flex-col items-center justify-center w-1/3 text-center">
+        <img alt="jkammellander" src="/img/jkammellander.png" />
+        <span class="text-2xl font-bold text-white">Jan Kammellander</span>
         <span class="text-gray-200">Frontend Developer & Grafik Designer</span>
       </div>
     </div>
@@ -48,7 +63,7 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from "@vue/runtime-core";
+import { defineComponent } from "@vue/runtime-core";
 
 export default defineComponent({
   setup() {
