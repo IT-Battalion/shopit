6bb78e14df2471b25fd7e6e8c481053e50cc511b
Jan Kammellander <janschwiderik@gmail.com>
2022-03-14 14:21:21 +0100

    new🎆: added vue3-html2pdf

--- resources/vue/views/EditCategories.vue
+++ resources/vue/views/EditCategories.vue
@@ -2,7 +2,15 @@
   <div class="grid justify-center h-full">
     <h1 class="mb-10 text-4xl font-bold text-center text-white">Kategorien</h1>
     <div
-      class="flex flex-row flex-wrap justify-center w-full mx-auto my-auto text-white  justify-items-center"
+      class="
+        flex flex-row flex-wrap
+        justify-center
+        w-full
+        mx-auto
+        my-auto
+        text-white
+        justify-items-center
+      "
       v-if="!isLoading"
     >
       <div
@@ -16,7 +24,7 @@
             v-if="!showInput.get(category.name)"
             @click="showInput.set(category.name, true)"
             class="cursor-pointer"
-          >{{ category.name }}</span
+            >{{ category.name }}</span
           >
           <template v-else>
             <input
@@ -32,21 +40,29 @@
                 showInput.set(category.name, false);
               "
             >
-              <img src="/img/check.svg" alt="check" class="w-8 h-8"/>
+              <img src="/img/check.svg" alt="check" class="w-8 h-8" />
             </button>
           </template>
           <button class="ml-5" @click="deleteCategory(category.id)">
-            <img src="/img/bin.svg" alt="delete" class="w-8 h-8"/>
+            <img src="/img/bin.svg" alt="delete" class="w-8 h-8" />
           </button>
         </div>
       </div>
     </div>
     <div
-      class="flex flex-row flex-wrap justify-center w-full mx-auto my-auto text-white  justify-items-center"
+      class="
+        flex flex-row flex-wrap
+        justify-center
+        w-full
+        mx-auto
+        my-auto
+        text-white
+        justify-items-center
+      "
       v-else
     >
       <div v-for="i in 5" :key="i" class="w-40 h-16 m-5">
-        <Skeletor :pill="true"/>
+        <Skeletor :pill="true" />
       </div>
     </div>
     <div class="w-full">
@@ -54,24 +70,30 @@
         Kategorie Hinzufügen
       </h2>
       <div class="flex flex-row items-center justify-center w-full gap-4">
-        <InputField ref="categorynamecreate"/>
+        <InputField ref="categorynamecreate" />
         <ButtonField class="mt-8" @click="createCategory">
-          <template v-slot:icon><img src="/img/addBlack.svg"/></template>
+          <template v-slot:icon><img src="/img/addBlack.svg" /></template>
         </ButtonField>
+        <DialogModal />
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
-import {Popover, PopoverButton, PopoverPanel} from "@headlessui/vue";
-import {endLoad, initLoad, state} from "../loader";
+import { defineComponent } from "vue";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
+import { endLoad, initLoad, state } from "../loader";
 import ButtonField from "../components/ButtonField.vue";
-import {useToast} from "vue-toastification";
-import {CreateCategoryRequest, EditCategoryRequest, ProductCategory,} from "../types/api";
-import {AxiosResponse} from "axios";
+import { useToast } from "vue-toastification";
+import {
+  CreateCategoryRequest,
+  EditCategoryRequest,
+  ProductCategory,
+} from "../types/api";
+import { AxiosResponse } from "axios";
 import InputField from "../components/InputField.vue";
+import DialogModal from "../components/DialogModal.vue";
 
 export default defineComponent({
   components: {
@@ -80,6 +102,7 @@ export default defineComponent({
     PopoverPanel,
     ButtonField,
     InputField,
+    DialogModal,
   },
   setup() {
     return {
@@ -110,8 +133,10 @@ export default defineComponent({
       initLoad();
       try {
         let name = (this.$refs.categoryname as HTMLInputElement[])[0].value;
-        let category = await this.$http.put<EditCategoryRequest,
-          AxiosResponse<ProductCategory>>(`/admin/category/${catID}`, {
+        let category = await this.$http.put<
+          EditCategoryRequest,
+          AxiosResponse<ProductCategory>
+        >(`/admin/category/${catID}`, {
           name,
         });
         this.categories.forEach((value, index, array) => {
@@ -149,8 +174,10 @@ export default defineComponent({
           this.$refs.categorynamecreate as typeof InputField
         ).getValue();
         console.log(name);
-        let category = await this.$http.post<CreateCategoryRequest,
-          AxiosResponse<ProductCategory>>(`/admin/category/`, {
+        let category = await this.$http.post<
+          CreateCategoryRequest,
+          AxiosResponse<ProductCategory>
+        >(`/admin/category/`, {
           name,
         });
         this.categories.push(category.data);
