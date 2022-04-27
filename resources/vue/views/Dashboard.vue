e922d628b4456785c7d0de5389d3fe468e1f0d90
pelias <pelias@student.tgm.ac.at>
2022-04-27 13:50:39 +0200

    Add: AGB & Impressum fields
    Signed-off-by: pelias <pelias@student.tgm.ac.at>

--- resources/vue/views/Dashboard.vue
+++ resources/vue/views/Dashboard.vue
@@ -1,13 +1,50 @@
 <template>
-  <h1 class="text-white font-bold text-6xl text-center">Coming Soon!</h1>
+  <div>
+    <h2 class="text-2xl font-bold text-white">
+      AGB
+    </h2>
+    <div>
+      <QuillEditor ref="agb" theme="snow" toolbar="essential"/>
+    </div>
+    <ButtonField class="px-6 py-4 mx-2" @click="save('agb')">
+      <template v-slot:text>Save</template>
+      <template v-slot:icon><img src="/img/doneBlack.svg"/></template>
+    </ButtonField>
+    <h2 class="text-2xl font-bold text-white">
+      Impressum
+    </h2>
+    <div>
+      <QuillEditor ref="impressum" theme="snow" toolbar="essential"/>
+    </div>
+    <ButtonField class="px-6 py-4 mx-2" @click="save('impressum')">
+      <template v-slot:text>Save</template>
+      <template v-slot:icon><img src="/img/doneBlack.svg"/></template>
+    </ButtonField>
+  </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "@vue/runtime-core";
+import {QuillEditor} from "@vueup/vue-quill";
+import {endLoad, initLoad} from "../loader";
 
 export default defineComponent({
-  setup() {
-    return {};
+  components: {QuillEditor},
+  data() {
+    return {
+      agbText: QuillEditor,
+      impressumText: QuillEditor,
+    }
+  },
+  async mounted() {
+    this.agbText = this.$refs.agb as typeof QuillEditor;
+    this.impressumText = this.$refs.impressum as typeof QuillEditor;
+  },
+  methods: {
+    async save(value: string) {
+      initLoad();
+      endLoad();
+    },
   },
 });
 </script>
