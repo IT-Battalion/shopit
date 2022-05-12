7edc8379e4bde851831cc7f761b7dbab2362156e
pdamianik <39028343+pdamianik@users.noreply.github.com>
2022-05-12 10:54:30 +0200

    Mail configuration
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

--- config/mail.php
+++ config/mail.php
@@ -43,6 +43,10 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
 //            'auth_mode' => 'null',
+
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
         ],
 
         'ses' => [
