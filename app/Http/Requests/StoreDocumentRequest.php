16daaa70ce2fa22223091de02da72f206bafbf2e
pdamianik <39028343+pdamianik@users.noreply.github.com>
2022-05-26 15:41:07 +0200

    Fix "Document" capitalization issue
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

--- app/Http/Requests/StoreDocumentRequest.php
+++ app/Http/Requests/StoreDocumentRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Auth;
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoredocumentRequest extends FormRequest
+class StoreDocumentRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
